a5f32d839ba8c1c20c0a48a085c9d4f340cb1361
Cesar Eduardo Arnetta <cesararnetta@gmail.com>
2025-06-29 07:58:46 +0000

    Donde teh frist 20 exercises

--- exercises/010-isSameLength/app.js
+++ exercises/010-isSameLength/app.js
@@ -1,1 +1,7 @@
 // Write your function here
+function isSameLength(w1,w2) {
+    if (w1.length == w2.length)
+        return true
+    else return false
+}
+console.log(isSameLength("w1","w22231"));
